880273bde54162defeb805b5ce7b6cfe38bf9e46
Rae Liu <happyaray@gmail.com>
2014-04-05 17:09:31 -0700

    update grab.sh to make it non-mac specific

--- grab.sh
+++ grab.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 url="http://ivod.ly.gov.tw/Play/VOD/734$1/300K"
-curl $url | grep -o "http:\/\/h264.*manifest.f4m" | pbcopy;
-php ./scripts/AdobeHDS.php --manifest "$(pbpaste)" --delete;
+php ./scripts/AdobeHDS.php --manifest "$(curl $url | grep -o 'http:\/\/h264.*manifest.f4m')" --delete;
